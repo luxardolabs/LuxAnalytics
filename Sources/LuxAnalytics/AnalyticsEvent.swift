956e5431f55c3922e1c5f9a259217cd7938c3515
luxardolabs <214140984+luxardolabs@users.noreply.github.com>
2025-06-02 11:10:40 -0500

    Add files via upload

--- Sources/LuxAnalytics/AnalyticsEvent.swift
+++ Sources/LuxAnalytics/AnalyticsEvent.swift
@@ -3,5 +3,13 @@ import Foundation
 public struct AnalyticsEvent: Codable {
     public let name: String
     public let timestamp: String
+    public let userId: String?
+    public let sessionId: String?
     public let metadata: [String: String]
+    
+    enum CodingKeys: String, CodingKey {
+        case name, timestamp, metadata
+        case userId = "user_id"
+        case sessionId = "session_id"
+    }
 }
